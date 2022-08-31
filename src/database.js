f69def01552d9efa1d975b01a54226113efca413
Oniros-six <lean.arrigonni@gmail.com>
2022-08-30 21:15:21 -0300

    Ahora la BD esta online en un cluster  de prueba

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/myApp';
+const URI = 'mongodb+srv://lean286:lealeo321@cluster0.juyk1f7.mongodb.net/test';
+// const URI = 'mongodb://localhost/myApp';
 
 mongoose.connect(URI)
         .then(db => console.log('DB is connected'))
