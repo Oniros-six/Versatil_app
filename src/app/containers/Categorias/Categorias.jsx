ced42553440ec61a7c2ca89c18b4a00289f8cb1e
Leandro Arrigonni <lean.arrigonni@gmail.com>
2022-10-11 14:31:09 -0300

    a ver

--- src/app/containers/Categorias/Categorias.jsx
+++ src/app/containers/Categorias/Categorias.jsx
@@ -41,7 +41,7 @@ const Categorias = () => {
 
     const getCategorias = async () => {
         try {
-            const categorias = await axios.get(`/api/categories/${user}`);
+            const categorias = await axios.get(`https://mi-app-leandro.herokuapp.com/api/categories/${user}`);
             setListaCategorias(categorias.data);
             setIdCat(categorias.data[0]._id)      
         } catch (error) {
