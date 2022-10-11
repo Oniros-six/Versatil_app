3c5fa6015cd3e6f9d65f1dda19b7718b10196968
Leandro Arrigonni <lean.arrigonni@gmail.com>
2022-10-11 13:07:03 -0300

    make it better

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const { config } = require('./config/config')
 const URI = config.dbUrl;
 
 mongoose.connect(URI)
-        .then(db => console.log('DB is connected'))
+        .then(db => console.log('DB is connected to ', URI))
         .catch(err => console.error(err));
 
 module.exports = mongoose;
