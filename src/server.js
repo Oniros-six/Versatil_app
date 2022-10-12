9bc469ea79901736994d740fa8156feb002f31a8
Leandro Arrigonni <lean.arrigonni@gmail.com>
2022-10-12 18:10:19 -0300

    revirtiendo la prueba de fallo

--- src/server.js
+++ src/server.js
@@ -41,7 +41,7 @@ app.use(boomErrorHandler);
 app.use(errorHandler);
 
 // Statics
-app.use(express.static(path.join(__dirname, '../pubc')));
+app.use(express.static(path.join(__dirname, '../public')));
 // Start server
 app.listen(process.env.PORT, () => {
     console.log(`The server is listening on the port ${app.get('port')}`)
